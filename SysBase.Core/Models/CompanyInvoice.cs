9e5cd2af1a5b705d4ae9e8d698c857ba317c32ef
EMRE ARIĞ <emre_arig@hotmail.com>
2024-12-05 12:07:23 +0300

    Create CompanyInvoice.cs

---
+++ SysBase.Core/Models/CompanyInvoice.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace SysBase.Core.Models
+{
+    public class CompanyInvoice : BaseEntity
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        [AllowHtml]
+        [DataType(DataType.MultilineText)]
+        public string FullAddress { get; set; }
+        public Company Company { get; set; }
+    }
+}
