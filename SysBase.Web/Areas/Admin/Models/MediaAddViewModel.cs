aa2247cdb257afaf81c083276ef603ae6403f65b
yunus-karaca-82 <83944694+yunus-karaca-82@users.noreply.github.com>
2024-12-27 12:54:02 +0300

    28.madde media menusu yapıldı

---
+++ SysBase.Web/Areas/Admin/Models/MediaAddViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SysBase.Core.Models;
+
+namespace SysBase.Web.Areas.Admin.Models
+{
+    internal class MediaAddViewModel
+    {
+        public MenuPermission MenuPermission { get; set; }
+        public Media Media { get; set; }
+        public IEnumerable<Language> Languages { get; set; }
+    }
+}
---
+++ SysBase.Web/Areas/Admin/Models/MediaListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using SysBase.Core.Models;
+
+namespace SysBase.Web.Areas.Admin.Models
+{
+    internal class MediaListViewModel
+    {
+        public MenuPermission MenuPermission { get; set; }
+        public List<Media> Medias { get; set; }
+    }
+}
